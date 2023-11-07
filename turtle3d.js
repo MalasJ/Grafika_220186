efd25902c34f1a14ba2df62e48c352bed685f1a8
MalasJ <147519825+MalasJ@users.noreply.github.com>
2023-11-07 01:49:25 +0100

    6 zrobione

--- turtle3d.js
+++ turtle3d.js
@@ -15,12 +15,12 @@ function forward(d){
     x=x+d*Math.sin(zenith)*Math.cos(azim);
     y=y+d*Math.sin(zenith)*Math.sin(azim);
     z=z+d*Math.cos(zenith);
-    bx=(x-z)*Math.sqrt(1/2);
-    by=x+2*y+z;
+    bx=(y-x)*Math.sqrt(3)/2+ maxX/2;
+    by=-(z-(x+y)/2) + maxY/2;
     if (pen==1){
         ctx.lineTo(bx,by);
         ctx.stroke();
-        //ctx.fillText(d,x-d*Math.cos(alfa)/2,y-d*Math.sin(alfa)/2);
+        //ctx.fillText(d+" "+(180/Math.PI*azim).toFixed(2)+" "+(180/Math.PI*zenith).toFixed(2),bx,by);
     }
     else{ctx.moveTo(bx,by);}
 }
@@ -39,12 +39,76 @@ function reset(){
     penUp();
     bx=maxX/2;
     by=maxY/2;
+    x=0;
+    y=0;
+    z=0;
     ctx.moveTo(bx,by);
-    zenith=0;
+    zenith=Math.PI/2;
     azim=0;
     changeColor("#000000");
 }
+function drawAxes(len,offset){
+    ctx.beginPath();
+    reset();
+    penDown();
+    forward(len);
+    penUp();
+    forward(10);
+    ctx.fillText("x",bx,by);
+    turn(Math.PI,0);
+    forward(10);
+    penDown();
+    forward(len+offset);
+    ///
+    reset();
+    penDown();
+    turn(Math.PI/2,0);
+    forward(len);
+    penUp();
+    forward(10);
+    ctx.fillText("y",bx,by);
+    turn(Math.PI,0);
+    forward(10);
+    penDown();
+    forward(len+offset);
+    ///
+    reset();
+    penDown();
+    turn(0,-Math.PI/2);
+    forward(len);
+    penUp();
+    forward(10);
+    ctx.fillText("z",bx,by);
+    turn(0,Math.PI);
+    forward(10);
+    penDown();
+    forward(len+offset);
+
+}
+drawAxes(200,70);
+
+ctx.beginPath();
 reset();
-//turn(Math.PI/4, Math.PI/6);
+forward(100);
 penDown();
-forward(10000);
+
+changeColor("blue");
+forward(50);
+turn(Math.PI/2,Math.PI/4);
+forward(50);
+turn(Math.PI/2,-Math.PI/4);
+forward(50);
+turn(Math.PI/2,-Math.PI/4);
+forward(50);
+
+ctx.beginPath();
+reset();
+turn(Math.PI*2/3,-Math.PI/12);
+forward(10);
+penDown();
+changeColor("green");
+forward(20);
+for (let i=0;i<60;i++){
+    turn(Math.PI/6,0);
+    forward(10);
+}
